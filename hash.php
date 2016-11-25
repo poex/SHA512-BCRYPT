6ac0f19f4a659a5361d41571fe3458db056a8bfa
poex <repo4432@gmail.com>
2016-11-25 02:26:14 +0200

    hashing

--- hash.php
+++ hash.php
@@ -1,10 +1,10 @@
 <?php
 // Creating SHA-512 hash of the user's password (DO NOT SAVE IT IN YOUR DATABASE)
 
+$password = 'what ever the user inputs';
 $sha512 = hash('SHA512', $password);
 
 // Creating a bcrypt hash
 
 $bcrypt = password_hash("$sha512", PASSWORD_DEFAULT);
-
 ?>
