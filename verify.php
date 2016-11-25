4bfd599d279e3a7be66883e79a61065e62978aac
poex <repo4432@gmail.com>
2016-11-25 02:29:27 +0200

    verify

---
+++ verify.php
@@ -0,0 +1,13 @@
+<?php
+// Creating SHA-512 hash of the user's password (DO NOT SAVE IT IN YOUR DATABASE)
+
+$password = 'what ever the user inputs';
+$sha512 = hash('SHA512', $password);
+
+// Creating a bcrypt hash
+
+$hash = '$2y$07$BCryptRequires22Chrcte/VlQH0piJtjXl.0t1XkA8pw9dMXTpOq'; // a hash from your database
+if (password_verify($sha512, $hash)) {
+	echo 'Password is correct';
+} else { echo 'Password is incorrect'; }
+?>
